b38cd6cb58ea4de475e8cde15c5e63b545de7dfd
SafiyaJ <Safiya_Jama@hotmail.co.uk>
2020-10-22 12:36:09 +0200

    Updates

--- main.js
+++ main.js
@@ -1,4 +1,118 @@
-var = "https://www.jsonstore.io/8ba4fd855086288421f770482e372ccb5a05d906269a34da5884f39eed0418a1";
-function getrandom(){ 
-    var random_string = Math.random().toString(32).substring(2, 5) + Math.random().toString(32).substring(2, 5);
-        return random_string()}
+const form = document.getElementById('shortURLForm');
+class ShortUrlAPI {
+    constructor(url) {
+      this.url = url;
+      this.hashID;
+      this.copyBtn;
+      this.shortUrl;
+    }
+  
+    async sendData() {
+      if (this.url == '') {
+        document.getElementById('responseError').style.display = 'block';
+        return;
+      } else {
+        var expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+        var regex = new RegExp(expression);
+        if (!this.url.match(regex)) {
+          document.getElementById('responseError').style.display = 'block';
+          return;
+        }
+      }
+      form.querySelector('button').classList.add('spinner');
+      const apiResponse = await fetch('https://rel.ink/api/links/', {
+        method: 'post',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          url: this.url,
+        }),
+      });
+      if (apiResponse.ok) {
+        this.hashID = await apiResponse.json();
+        let getHashIDS = localStorage.getItem('hashids');
+        getHashIDS = getHashIDS ? getHashIDS.split(',') : [];
+        getHashIDS.push(this.hashID.hashid);
+        localStorage.setItem('hashids', getHashIDS.toString());
+        this.updateUI();
+      } else {
+        form.querySelector('button').classList.remove('spinner');
+        throw 'Something went wrong! Try Again';
+      }
+    }
+  
+    updateUI() {
+      document.querySelector('.shortUrl-container').style.display = 'block';
+      const urlEl = document.createElement('div');
+      urlEl.className = 'shortUrl-box';
+      const urlTemplate = `<div class="shortUrl--link">${this.url}</div>
+      <hr />
+      <div class="shortUrl--refLink">https://rel.ink/${this.hashID.hashid}</div>`;
+      urlEl.innerHTML = urlTemplate;
+      this.shortUrl = `https://rel.ink/${this.hashID.hashid}`;
+      this.copyBtn = document.createElement('button');
+      this.copyBtn.className = 'button';
+      this.copyBtn.textContent = 'Copy';
+      this.copyBtn.addEventListener(
+        'click',
+        this.copyBtnHandler.bind(this, this.shortUrl)
+      );
+      urlEl.insertAdjacentElement('beforeend', this.copyBtn);
+      document.querySelector('.shortUrl-container').appendChild(urlEl);
+      form.reset();
+      document.getElementById('responseError').style.display = 'none';
+      form.querySelector('button').classList.remove('spinner');
+    }
+  
+    async copyBtnHandler(shortUrl) {
+      await navigator.clipboard.writeText(shortUrl);
+      this.copyBtn.textContent = 'Copied!';
+      this.copyBtn.classList.add('copied');
+      setTimeout(() => {
+        this.copyBtn.classList.remove('copied');
+        this.copyBtn.textContent = 'Copy';
+      }, 1000);
+    }
+  }
+  
+  class LocalUrl extends ShortUrlAPI {
+    constructor() {
+      super(false);
+      let getHashIDS = localStorage.getItem('hashids');
+      if (!getHashIDS) {
+        return;
+      }
+      getHashIDS = getHashIDS.split(',');
+      getHashIDS.forEach((hashID) => {
+        this.hashID = { hashid: hashID };
+        this.fetchData();
+      });
+    }
+  
+    async fetchData() {
+      const apiResponse = await fetch(
+        'https://rel.ink/api/links/' + this.hashID.hashid + '/'
+      );
+      if (apiResponse.ok) {
+        const data = await apiResponse.json();
+        this.url = data.url;
+        this.updateUI();
+      } else {
+        throw 'Something went wrong! Try Again';
+      }
+    }
+  }
+  
+  class App {
+    static init() {
+      new LocalUrl();
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const newURL = form.querySelector('input').value;
+        new ShortUrlAPI(newURL).sendData();
+      });
+    }
+  }
+  App.init();
